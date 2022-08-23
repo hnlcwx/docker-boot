bd10da1206a8640ac0f181738dd0c257aad72d43
010219 <123456789>
2022-08-23 17:29:38 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/test/docker")
+    public String helloDocker(){
+        return "hello docker"+"\t"+port+"\t"+ UUID.randomUUID().toString();
+    }
+
+    @RequestMapping("/test/index")
+    public String index(){
+        return "服务端口号："+"\t"+port+"\t"+ UUID.randomUUID().toString();
+    }
+}
