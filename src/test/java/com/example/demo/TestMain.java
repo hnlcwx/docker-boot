ee98a1fdaff9d6a1a128b17460fe1dd176c79a75
010219 <123456789>
2022-08-24 09:16:20 +0800

    hot commit2

--- src/test/java/com/example/demo/TestMain.java
+++ src/test/java/com/example/demo/TestMain.java
@@ -6,6 +6,7 @@ public class TestMain {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
         System.out.println("hot-test");
     }
 }
